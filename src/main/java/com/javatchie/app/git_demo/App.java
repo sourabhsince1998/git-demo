08c6d0f5b175597188696aad68f09f976e4f47f2
sourabhsince1998 <sourabhsince1998@gmail.com>
2023-02-21 14:04:28 +0530

    added if condition to check data

--- src/main/java/com/javatchie/app/git_demo/App.java
+++ src/main/java/com/javatchie/app/git_demo/App.java
@@ -6,6 +6,14 @@ package com.javatchie.app.git_demo;
  */
 public class App 
 {
+	public void addData(String data)
+	{
+		System.out.println("data ="+data);
+		if(data.equals("java techie")) {
+			System.out.println("welcome");
+		}
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
